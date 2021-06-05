3c5c447e2b8c8d839620f2e7740a54b35cae985f
Caio Beneduci <caio.beneduci@icloud.com>
2021-06-05 03:19:02 -0300

    integração da pagina com webservice

--- src/app/meals/new-meal/new-meal.page.ts
+++ src/app/meals/new-meal/new-meal.page.ts
@@ -1,8 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-import {Food} from 'src/app/typings/types';
+// import {Food} from 'src/app/typings/types';
+
 import {FoodList} from 'src/app/offline_data/data';
 import {homeList} from 'src/app/offline_data/data';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
+interface Food {
+  _id : string;
+  Nome : string;
+  Calorias : number;
+};
+
+interface UserFood {
+  Nome : string;
+  Calorias : number;
+  Quantidade : number;
+};
 
 @Component({
   selector: 'app-new-meal',
@@ -11,69 +25,108 @@ import {homeList} from 'src/app/offline_data/data';
 })
 
 export class NewMealPage implements OnInit {
+
+  public allFoods : Food[];
+
+  public selectedFoods = [];
+
+  public userFoodlist : UserFood[];
+
+  constructor(private http: HttpClient, private router: Router) { }  
   
-  public foodList = FoodList;
+  private async getFoods () {
+    const url = "https://maissaudews.azurewebsites.net/api/listar_alimentos?code=779To5zyxi4pxMgPcCqaEOWms23tZFjPr7VtjyThdnCZVuiuX24fOA%3D%3D";
+    const body = {}
+    const result = await this.http.post<Food[]>(url,body).toPromise();
+    this.allFoods = result
+    
+  }
 
-  public foods: Food[] = []
-
-  public selectedFoods = []
+  private async saveFoods (body) {
+    const url = "https://maissaudews.azurewebsites.net/api/criar_refeicao?code=N7DQG2DjirAOGVgixaZhRLYtH6sTp/KFNbyVGWe6Mv9zFZQS5G4SEA==";
+    this.http.post(url,body).toPromise();
+    
+  }
 
   public selecteditens() {
-
-    const foods: Food[] = []
     
-    this.selectedFoods.forEach(function (selectedFoodItem) {
+    const userFoodlist : UserFood[] = [];
+    const allFoods : Food[] = this.allFoods;
+    const selectedFoods = this.selectedFoods;
 
-      FoodList.forEach(function (foodListItem) {
+    allFoods.forEach(function (allFood) {
+    
+      selectedFoods.forEach(function (selectedFood) {
 
-        if (selectedFoodItem == foodListItem.id) {
-          foods.push({id: foodListItem.id, name: foodListItem.name, quantity: 50})
+        if (selectedFood == allFood._id) {
+          userFoodlist.push({Nome: allFood.Nome, Calorias: allFood.Calorias, Quantidade : 100})
         }
 
       })
       
-    } 
-    )
+    })
 
-    this.foods = foods
+    this.userFoodlist = userFoodlist
   
+  }
+
+  public changeFoodQuantity(index, type) {
+
+    if (type == 'add') {
+      this.userFoodlist[index].Quantidade += 50;
+    }
+    if (type == 'sub' && this.userFoodlist[index].Quantidade >=50) {
+      this.userFoodlist[index].Quantidade -= 50;
+    }
+
   }
 
   public save(form) {
 
-    var foods = ''
-
-    this.foods.forEach(function (value) {
-      foods = foods + ',' + value.name
+    this.userFoodlist.forEach(function (food) {
+      food.Calorias = food.Quantidade / 100 * food.Calorias
     })
 
-    homeList.push(
+    const body = {
+        "user_id" : "1",
+        "tipo_refeicao" : form.value.MealName,
+        "data_refeicao" : form.value.MealDate.substr(8,2) + '/' + form.value.MealDate.substr(5,2) + '/' + form.value.MealDate.substr(0,4),
+        "hora_refeicao" : form.value.MealTime.substr(11,5),
+        "alimentos" : this.userFoodlist
+    }
+
+    this.saveFoods(body)
+    
+    this.router.navigate(['/home'], {replaceUrl: true})
+    
+    
+    
+    // homeList.push(
       
-      {
-        title: form.value.MealName + ' ' + form.value.MealDate.substr(8,2) + '/' + form.value.MealDate.substr(5,2) + '/' + form.value.MealDate.substr(0,4) ,
-        caloriesConsumed: {
-          value: '1347 Kcal',
-          label: 'Kcal consumidas'
-        },
-        time: {
-          value: form.value.MealTime.substr(11,5),
-          label: 'Horário'
-        },
-        foods: {
-          value: foods,
-          label: 'Alimentos consumidos'
-        }
-      }
-    )
+    //   {
+    //     title: form.value.MealName + ' ' + form.value.MealDate.substr(8,2) + '/' + form.value.MealDate.substr(5,2) + '/' + form.value.MealDate.substr(0,4) ,
+    //     caloriesConsumed: {
+    //       value: '1347 Kcal',
+    //       label: 'Kcal consumidas'
+    //     },
+    //     time: {
+    //       value: form.value.MealTime.substr(11,5),
+    //       label: 'Horário'
+    //     },
+    //     foods: {
+    //       value: foods,
+    //       label: 'Alimentos consumidos'
+    //     }
+    //   }
+    // )
     
   }
 
-  constructor() { }
+  
 
   ngOnInit() {
+    this.getFoods();
 
-    console.log(this.selectedFoods);
-    console.log(this.foodList)
   }
 
  
