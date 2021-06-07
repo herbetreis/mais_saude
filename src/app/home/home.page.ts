d5e2aa4f4bcea1b39470ea53773157d05e2d6711
Caio Beneduci <caio.beneduci@icloud.com>
2021-06-06 23:14:43 -0300

    ajustes card acompanhamento

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -34,33 +34,33 @@ export class HomePage {
       this.meals = [...(result && Array.isArray(result.meals) ? result.meals : [])];
       this.dashboardData = { ...this.dashboardData, ...(result && typeof result.summary === 'object' ? result.summary : {} as Summary) };
 
-      const totalConsumed = this.meals.reduce((acc, curr) => {
-        if (curr?.caloriesConsumed && typeof curr.caloriesConsumed.value === 'string') {
-          const [strVal] = curr.caloriesConsumed.value.split(' ');
-          if (!isNaN(parseFloat(strVal))) {
-            return acc + parseFloat(strVal);
-          }
-          return acc;
-        }
-        return acc;
-      }, 0);
-      const [aux] = typeof this.dashboardData?.caloriesTarget?.value === 'string' ? this.dashboardData.caloriesTarget.value.split(' ') : ['0'];
-      const caloriesTarget = !isNaN(parseFloat(aux)) ? parseFloat(aux) : 0
+      // const totalConsumed = this.meals.reduce((acc, curr) => {
+      //   if (curr?.caloriesConsumed && typeof curr.caloriesConsumed.value === 'string') {
+      //     const [strVal] = curr.caloriesConsumed.value.split(' ');
+      //     if (!isNaN(parseFloat(strVal))) {
+      //       return acc + parseFloat(strVal);
+      //     }
+      //     return acc;
+      //   }
+      //   return acc;
+      // }, 0);
+      // const [aux] = typeof this.dashboardData?.caloriesTarget?.value === 'string' ? this.dashboardData.caloriesTarget.value.split(' ') : ['0'];
+      // const caloriesTarget = !isNaN(parseFloat(aux)) ? parseFloat(aux) : 0
 
-      const caloriesConsumed = {
-        ...this.dashboardData.caloriesConsumed,
-        value: `${totalConsumed} Kcal`
-      };
-      const caloriesPercent = {
-        ...this.dashboardData.caloriesPercent,
-        value: `${((totalConsumed / caloriesTarget)*100).toFixed(2)}%`
-      };
+      // const caloriesConsumed = {
+      //   ...this.dashboardData.caloriesConsumed,
+      //   value: `${totalConsumed} Kcal`
+      // };
+      // const caloriesPercent = {
+      //   ...this.dashboardData.caloriesPercent,
+      //   value: `${((totalConsumed / caloriesTarget)*100).toFixed(2)}%`
+      // };
 
-      this.dashboardData = {
-        ...this.dashboardData,
-        caloriesPercent,
-        caloriesConsumed
-      }
+      // this.dashboardData = {
+      //   ...this.dashboardData,
+      //   caloriesPercent,
+      //   caloriesConsumed
+      // }
     }
   }
 
