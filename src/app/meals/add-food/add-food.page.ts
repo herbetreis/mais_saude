f989aac04f3c7870b8cd5fa614fde6776ff68a42
Caio Beneduci <caio.beneduci@icloud.com>
2021-06-06 22:15:17 -0300

    Integração com API

--- src/app/meals/add-food/add-food.page.ts
+++ src/app/meals/add-food/add-food.page.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-food',
@@ -7,7 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddFoodPage implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router) { }
+
+  public async save(form) {
+
+   console.log(form)
+
+    if (form.value.measureUnit == 2){
+      form.value.weight *= 1000
+    }
+
+    const body = {
+      'Nome': form.value.foodTitle,
+      'Calorias': Math.round((100 * form.value.calories) / form.value.weight)
+    };
+
+    const url = 'https://maissaudews.azurewebsites.net/api/criar_alimento?code=N7DQG2DjirAOGVgixaZhRLYtH6sTp/KFNbyVGWe6Mv9zFZQS5G4SEA==';
+    await this.http.post(url, body).toPromise();
+
+    return this.router.navigate(['/home'], { replaceUrl: true });
+
+  }
 
   ngOnInit() {
   }
